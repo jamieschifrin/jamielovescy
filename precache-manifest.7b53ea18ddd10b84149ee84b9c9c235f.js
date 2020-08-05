7f1c6c60040f85ce60597cb83dcbd53cba1dc363
Jamie Schrifrin <jamieschifrin@hotmail.com>
2020-08-05 15:51:02 -0400

    Updates

---
+++ precache-manifest.7b53ea18ddd10b84149ee84b9c9c235f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d54cc49ae4429fe277ea12e5819a6d98",
+    "url": "/jamielovescy/index.html"
+  },
+  {
+    "revision": "5cf9c8e0e7bf75bebb88",
+    "url": "/jamielovescy/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "7b15efa723b9b65a41e9",
+    "url": "/jamielovescy/static/js/2.35a15de7.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/jamielovescy/static/js/2.35a15de7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5cf9c8e0e7bf75bebb88",
+    "url": "/jamielovescy/static/js/main.eedba03d.chunk.js"
+  },
+  {
+    "revision": "4d179b0653f8ad88b85d",
+    "url": "/jamielovescy/static/js/runtime-main.42ebb21b.js"
+  },
+  {
+    "revision": "a3fb495c16f00b5e100e50264c9f3d04",
+    "url": "/jamielovescy/static/media/cy.a3fb495c.jpg"
+  }
+]);
